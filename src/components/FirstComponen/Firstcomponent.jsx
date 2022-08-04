7af9408c480ce6dedd3c5f22b71735e012888a66
Volodymyr Kocherhan <volodymyrkocherhan@gmail.com>
2022-08-04 22:58:43 +0300

    preStart

---
+++ src/components/FirstComponen/Firstcomponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './FirstComponen.css';
+// import styles from './FirstComponen.css';
+
+const FirstComponent = props => {
+  return (
+    <>
+      <div className="size">First Componen</div>
+      {/* <div className={styles.size}>First Componen</div> */}
+    </>
+  );
+};
+
+export default FirstComponent;
