7f4c2aa18450b4d0f523454c9b73600e4f183d95
Воронюк Олена <olena.voroniuk1@gmail.com>
2022-12-09 21:00:11 +0200

    not SlideShow

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,26 @@
-import React from "react";
-import AccordionList from "../../components/Accordion/AccordionList";
-import Header from "../../components/Header/Header";
+import React from 'react';
+import style from './HomePage.module.css';
+//import Navigation from '../../shared/Navigation/Navigation';
+import Header from '../../components/Header/Header';
+import Home from '../../components/Home/Home.jsx';
 
-const HomePage = () => {
+const HomePage = (props) => {
 
   return (
     <>
-    <Header/>
+          <div className={style.pageMain}>
+          <div className={style.visibleLg}>
+            <Header  />
+          </div>
+
+          <div className={style.pageOutlet}>
+             <Home /> 
+          </div>
+
+        </div>
+           
     </>
-  )
-}
+  );
+};
+
 export default HomePage;
