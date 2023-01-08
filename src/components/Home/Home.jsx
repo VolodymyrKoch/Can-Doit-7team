ecf4e54eb9faa6346fb43f9fde6b4caa6feecaec
Воронюк Олена <olena.voroniuk1@gmail.com>
2023-01-08 23:54:38 +0200

    slideShow2

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,7 @@
 import React from 'react';
+//import CarouselComponent from '../CarouselBox/CarouselBox.jsx';
 import style from './home.module.css';
-//import Carousel from 'react-bootstrap/Carousel';
-//import 'bootstrap/dist/css/bootstrap.min.css';
 import imageMax1 from '../../image/image1Max.jpg';
-import imageMax2 from '../../image/image2Max.jpg';
-import imageMax3 from '../../image/image3Max.jpg';
-import imageMax4 from '../../image/image4Max.jpg';
-import imageMax5 from '../../image/image5Max.jpg';
-import imageMax6 from '../../image/image6Max.jpg';
 
 const Home = props => {
 
@@ -24,68 +18,29 @@ const Home = props => {
                     </h2>
                 </div>
                     
-                <div className={style.carouselBox}>
-                    {/* <Carousel>
-                        <Carousel.Item>
-                            <div >
-                                <img
-                                src={imageMax1}
-                                alt='imageMax1'
-                                />
-
-                                <img
-                                src={imageMax2}
-                                alt='imageMax1'
-                                />
-
-                                <img
-                                src={imageMax3}
-                                alt='imageMax1'
-                                />
-                            </div>
-                            
-                        </Carousel.Item>
-
-                        <Carousel.Item>
-                            <div>
-                            <img
-                                src={imageMax4}
-                                alt='imageMax2'
-                            />
-                            <img
-                                src={imageMax5}
-                                alt='imageMax2'
-                            />
-                            <img
-                                src={imageMax6}
-                                alt='imageMax2'
-                            />
-                            </div>
-                        </Carousel.Item>
-                    </Carousel> */}
-                 
-                </div>
-               
-                
+                {/* <div className={style.carouselBox}>
+                     <CarouselComponent />                                            
+                </div> */}
+                               
                 <div className={style.logo}>
                     <img src={imageMax1} alt="" />
                 </div>
 
 
-                <div className={style.textItem}>
+                <div className={style.textHome}>
 
-                    <div className={style.textItem}>Вебзастосунок «CanDoIt» - Ваш мініпомічник в екстрених ситуаціях, де в 2-4 кроках описано, як діяти за певних обставин до прибуття швидкої допомоги чи МНС.
+                    <p > <b className={style.phone}>Вебзастосунок «CanDoIt» </b>- Ваш мініпомічник в екстрених ситуаціях, де в 2-4 кроках описано, як діяти за певних обставин до прибуття швидкої допомоги чи МНС.
 
-                    </div>
+                    </p>
                     
-                    <div className={style.textItem}>Паніка - страшний ворог, але конкретні дії, іноді навіть мінімальні, можуть врятувати життя не лише своє, а й оточуючих.
+                    <p>Паніка - страшний ворог, але конкретні дії, іноді навіть мінімальні, можуть врятувати життя не лише своє, а й оточуючих.
     Задимленість, кровотеча, осколкові поранення, опіки, втрата свідомості, перебування під завалами будинку і т. д. - чималий перелік ситуацій, у які потрапляють українці щодня.
 
-                    </div>
+                    </p>
                 
-                    <div className={style.textItem}>Завдання застосунку - вказати конкретні дії, які потрібно виконати в певній надзвичайній ситуації і які зможуть виконати навіть діти.
+                    <p >Завдання застосунку - вказати конкретні дії, які потрібно виконати в певній надзвичайній ситуації і які зможуть виконати навіть діти.
 
-                    </div>
+                    </p>
 
                     <br></br>
                         <ul className={style.list}>
