c101bd2393c86beb11d48a94fdb132a812827118
Volodymyr Kocherhan <volodymyrkocherhan@gmail.com>
2023-07-24 13:18:23 +0300

    fix rout

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import styles from './MainPage.module.css';
-import { Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { Layout } from '../Layout/Layout.jsx';
 import EmergencyListPage from '../EmergencyListPage/EmergencyListPage';
 import EmergencyPage from '../EmergencyPage/EmergencyPage';
@@ -8,56 +8,55 @@ import EvacuationPage from '../EvacuationPage/EvacuationPage';
 import AlarmingSuitcasePage from '../AlarmingSuitcasePage/AlarmingSuitcasePage.jsx';
 import { SearchContext } from '../../shared/search-context';
 import { IdContext } from '../../shared/Context/IdContext';
-import { EmergencyContext} from '../../shared/Context/EmergencyContext'
+import { EmergencyContext } from '../../shared/Context/EmergencyContext';
 import HomePage from '../HomePage/HomePage';
 import NotFound from '../NotFound/NotFound';
 import EmergencyList from '../../components/EmergencyList/EmergencyList';
 import { IsActiveContext } from '../../shared/Context/isActiveContext';
 
-
-
-
 const MainPage = function () {
-
   const [searchValue, setSearchValue] = useState('');
   const [idSearch, setIdSearch] = useState([]);
   const [emergency, setEmergency] = useState();
   const [isActiveLink, setIsActiveLink] = useState(null);
 
-useEffect(() => {
-  console.log('emergency',emergency)
-})
+  useEffect(() => {
+    console.log('emergency', emergency);
+  });
 
   return (
     <>
       <div className={styles.bgContainer}>
         <SearchContext.Provider value={searchValue}>
-            <IdContext.Provider value={{idSearch, setIdSearch}}>
-              <EmergencyContext.Provider value={{emergency, setEmergency}}>
-              <IsActiveContext.Provider value={{ isActiveLink, setIsActiveLink }}>
-                  <Routes>
-    
-                    <Route path="/" element={<Layout />}>
+          <IdContext.Provider value={{ idSearch, setIdSearch }}>
+            <EmergencyContext.Provider value={{ emergency, setEmergency }}>
+              <IsActiveContext.Provider
+                value={{ isActiveLink, setIsActiveLink }}
+              >
+                <Routes>
+                  <Route path="/" element={<Layout />}>
+                    <Route index element={<HomePage />} />
+                    <Route path="can-doit-7team" element={<HomePage />} />
 
-                        <Route index element={<HomePage />}/>
-                      
-                        <Route path="Category"  element={<EmergencyListPage />}>
-
-                            <Route path=":id" element={<EmergencyList/>}/>
-                            <Route path="Emergency-:emergencyid" element={<EmergencyPage />}/> 
-
-                        </Route>
-
-                        <Route path="EvacuationPage" element={<EvacuationPage />}/>
-                        <Route path="AlarmingSuitcasePage" element={<AlarmingSuitcasePage />}/>
-                        <Route path="*" element={<NotFound/> } />
-
+                    <Route path="Category" element={<EmergencyListPage />}>
+                      <Route path=":id" element={<EmergencyList />} />
+                      <Route
+                        path="Emergency-:emergencyid"
+                        element={<EmergencyPage />}
+                      />
                     </Route>
 
+                    <Route path="EvacuationPage" element={<EvacuationPage />} />
+                    <Route
+                      path="AlarmingSuitcasePage"
+                      element={<AlarmingSuitcasePage />}
+                    />
+                    <Route path="*" element={<NotFound />} />
+                  </Route>
                 </Routes>
-                </IsActiveContext.Provider>
-              </EmergencyContext.Provider>
-            </IdContext.Provider>
+              </IsActiveContext.Provider>
+            </EmergencyContext.Provider>
+          </IdContext.Provider>
         </SearchContext.Provider>
       </div>
     </>
